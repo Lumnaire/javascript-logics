10d35a8ef70b4a33a06377f53455be5c049c3918
Ronald Castromero <callme14noy@gmail.com>
2025-08-09 21:56:25 +0800

    some changes

--- 02_Fundamentals_Part_1/Compare_Objects_for_Equivalent_Properties/script.js
+++ 02_Fundamentals_Part_1/Compare_Objects_for_Equivalent_Properties/script.js
@@ -6,4 +6,4 @@ const matches = (obj, source) =>
 console.log(matches({ age: 25, hair: 'long', beard: true }, { hair: 'long', beard: true })); // true
 console.log(matches({ hair: 'long', beard: true }, { age: 25, hair: 'long', beard: true })); // false
 console.log(matches({ hair: 'long', beard: true }, { age: 26, hair: 'long', beard: true })); // false
-console.log(matches({ age: 24, hair: 'long', beard: true }, { hair: 'long', beard: false })); // true
+console.log(matches({ age: 24, hair: 'long', beard: true }, { hair: 'long', beard: false })); // false
